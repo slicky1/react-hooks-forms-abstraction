3d61c71f50f78db6f0e4375c2e50867051fa00aa
slicky1 <okunonke@gmail.com>
2021-05-23 21:23:53 -0400

    assigment done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,24 +1,75 @@
 import React, { useState } from "react";
 
 function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
+  const [formData, setFormData] = useState({
+    firstName: "Mike",
+    lastName: "Okunonke",
+    admin: false,
+  })
+  //const [lastName, setLastName] = useState("Henry");
 
-  function handleFirstNameChange(event) {
-    setFirstName(event.target.value);
+  function handleChange(event) {
+    // name is the KEY in of the formData object we're trying to update
+    const name = event.target.name;
+    let value = event.target.value;
+  
+    if (event.target.type === "checkbox") {
+      value = event.target.checked;
+    }
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  } 
+
+  // function handleFirstNameChange(event) {
+  //   setFormData({
+  //     ...formData,
+  //     firstName: event.target.value,
+  //   });
+  //   //setFirstName(event.target.value);
+  // }
+
+  // function handleLastNameChange(event) {
+  //   setFormData({
+  //     ...formData,
+  //     LastName: event.target.value,
+  //   });
+  //   //setLastName(event.target.value);
+  // }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
   }
+  
 
-  function handleLastNameChange(event) {
-    setLastName(event.target.value);
-  }
+  return(
+    <form onSubmit ={handleSubmit}>
+      <input
+      type = "text"
+      name = "firstName"
+      onChange = {handleChange}
+      value = {FormData.firstName}
+      />
 
-  return (
-    <form>
-      <input type="text" onChange={handleFirstNameChange} value={firstName} />
-      <input type="text" onChange={handleLastNameChange} value={lastName} />
-      <button type="submit">Submit</button>
+<input
+      type = "text"
+      name = "lastName"
+      onChange = {handleChange}
+      value = {FormData.lastName}
+      />
+
+<input
+      type = "checkbox"
+      name = "admin"
+      onChange = {handleChange}
+      value = {FormData.admin}
+      />
+<button type="submit">Submit</button>
+
     </form>
-  );
+  )
 }
 
 export default Form;
